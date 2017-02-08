24c95ace3c90893aa3b99b4368b4c827444ba3ce
robertrancz <rancz.robi@gmail.com>
2017-02-08 23:25:20 +0200

    Add route guard for the product detail Id - Product detail Id shall be an integer greater than one

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,6 +9,7 @@ import { WelcomeComponent } from './home/welcome.component';
 
 import { ProductListComponent } from './products/product-list.component';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductDetailGuard } from './products/product-guard.service';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
 
@@ -19,7 +20,7 @@ import { StarComponent } from './shared/star.component';
     HttpModule,
     RouterModule.forRoot([
         {path: 'products', component: ProductListComponent },
-        { path: 'product/:id', component: ProductDetailComponent },
+        { path: 'product/:id', component: ProductDetailComponent, canActivate: [ ProductDetailGuard ] },
         { path: 'welcome', component: WelcomeComponent },
         { path: '', redirectTo: 'welcome', pathMatch: 'full' },
         { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
@@ -33,6 +34,7 @@ import { StarComponent } from './shared/star.component';
     ProductFilterPipe,
     StarComponent
      ],
+  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
