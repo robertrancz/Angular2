d65800c38dbed266fec18cfd9f9a2930d234cd00
robertrancz <rancz.robi@gmail.com>
2017-02-08 22:44:30 +0200

    Passing the product id parameter to the product detail route

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IProduct } from './product';
+
+@Component({
+    templateUrl: 'app/products/product-detail.component.html',
+    styleUrls: ['app/products/product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit{
+    pageTitle: string = 'Product Detail';
+    product: IProduct;
+
+    constructor(private _route: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
+}
