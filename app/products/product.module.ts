5048f01ff93585fe579b398b5f867c08533b3488
robertrancz <rancz.robi@gmail.com>
2017-02-14 21:44:03 +0200

    Separate the StarComponent as a shared module

--- app/products/product.module.ts
+++ app/products/product.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductDetailGuard } from './product-guard.service';
 import { ProductFilterPipe } from './product-filter.pipe';
-import { StarComponent } from '../shared/star.component';
-
 import { ProductService } from './product.service';
+
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
-        ProductFilterPipe,
-        StarComponent
+        ProductFilterPipe
     ],
     imports: [
-        FormsModule,
-        CommonModule,
+        SharedModule,
         RouterModule.forChild([
             {path: 'products', component: ProductListComponent },
             { path: 'product/:id', component: ProductDetailComponent, canActivate: [ ProductDetailGuard ] }
