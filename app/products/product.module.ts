2a21b04accc03eb2069bbf5b41abbb8705985cb3
robertrancz <rancz.robi@gmail.com>
2017-02-14 21:29:16 +0200

    Add product feature module

---
+++ app/products/product.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailGuard } from './product-guard.service';
+import { ProductFilterPipe } from './product-filter.pipe';
+import { StarComponent } from '../shared/star.component';
+
+import { ProductService } from './product.service';
+
+@NgModule({
+    declarations: [
+        ProductListComponent,
+        ProductDetailComponent,
+        ProductFilterPipe,
+        StarComponent
+    ],
+    imports: [
+        FormsModule,
+        CommonModule,
+        RouterModule.forChild([
+            {path: 'products', component: ProductListComponent },
+            { path: 'product/:id', component: ProductDetailComponent, canActivate: [ ProductDetailGuard ] }
+        ])
+    ],
+    providers: [
+        ProductService,
+        ProductDetailGuard
+    ]
+})
+export class ProductModule {}
